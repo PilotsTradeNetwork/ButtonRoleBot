f12254838694a902342072d3d5768af0de95cde6
cimspin <80884597+cimspin@users.noreply.github.com>
2023-09-19 21:48:46 +0100

    Create bot skeleton
    Resolves #1

---
+++ ptn/buttonrolebot/_metadata.py
@@ -0,0 +1,2 @@
+# version is BREAKING CHANGE - MAJOR CHANGE - MINOR CHANGE
+__version__ = '0.1.0'
---
+++ ptn/buttonrolebot/modules/Helpers.py
@@ -0,0 +1,60 @@
+"""
+A module for helper functions called by other modules.
+
+Depends on: ErrorHandler
+
+"""
+
+# import discord.py
+import discord
+from discord import app_commands
+
+# import local modules
+from ptn.buttonrolebot.modules.ErrorHandler import CommandRoleError
+
+
+"""
+PERMISSION CHECKS
+
+Used for application commands
+"""
+
+# trio of helper functions to check a user's permission to run a command based on their roles, and return a helpful error if they don't have the correct role(s)
+def getrole(ctx, id): # takes a Discord role ID and returns the role object
+    role = discord.utils.get(ctx.guild.roles, id=id)
+    return role
+
+async def checkroles_actual(interaction: discord.Interaction, permitted_role_ids):
+    try:
+        """
+        Check if the user has at least one of the permitted roles to run a command
+        """
+        print(f"checkroles called.")
+        author_roles = interaction.user.roles
+        permitted_roles = [getrole(interaction, role) for role in permitted_role_ids]
+        print(author_roles)
+        print(permitted_roles)
+        permission = True if any(x in permitted_roles for x in author_roles) else False
+        print(permission)
+        return permission, permitted_roles
+    except Exception as e:
+        print(e)
+    return permission
+
+
+def check_roles(permitted_role_ids):
+    async def checkroles(interaction: discord.Interaction):
+        permission, permitted_roles = await checkroles_actual(interaction, permitted_role_ids)
+        print("Inherited permission from checkroles")
+        if not permission: # raise our custom error to notify the user gracefully
+            role_list = []
+            for role in permitted_role_ids:
+                role_list.append(f'<@&{role}> ')
+                formatted_role_list = " • ".join(role_list)
+            try:
+                raise CommandRoleError(permitted_roles, formatted_role_list)
+            except CommandRoleError as e:
+                print(e)
+                raise
+        return permission
+    return app_commands.check(checkroles)
